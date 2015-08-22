423ce6f2c97f9fff4de96c32ee7477e378dc5b3f
Muhammad Fred <muhfred@outlook.com>
2015-08-22 02:43:38 +0200

    critical bug

---
+++ target/generated-sources/annotations/entity/Product_.java
@@ -0,0 +1,23 @@
+package entity;
+
+import entity.Category;
+import entity.OrderedProduct;
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.CollectionAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-08-22T03:38:22")
+@StaticMetamodel(Product.class)
+public class Product_ { 
+
+    public static volatile SingularAttribute<Product, BigDecimal> price;
+    public static volatile SingularAttribute<Product, Date> lastUpdate;
+    public static volatile SingularAttribute<Product, String> name;
+    public static volatile SingularAttribute<Product, Integer> id;
+    public static volatile SingularAttribute<Product, Category> category;
+    public static volatile CollectionAttribute<Product, OrderedProduct> orderedProductCollection;
+
+}
